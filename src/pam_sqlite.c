40649ea893e50383e6b01357533fd158f680e4fd
vincent <vincent.filou@gmail.com>
2019-11-25 05:39:11 +0000

    cleanup

--- src/pam_sqlite.c
+++ src/pam_sqlite.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <strings.h>
@@ -16,6 +15,10 @@
 #define QUERY_STRING "SELECT * from Users WHERE Username=?1"
 #define UPDATE_STRING "UPDATE Users SET Password=?1 WHERE Username=?2"
 #define LOG_STRING "INSERT INTO Logs VALUES( ?1, CURRENT_TIMESTAMP, ?2)"
+#define AUTH_SQLITE_ERROR_STRING "Unable to authenticate user. SQLITE-ERROR: %s"
+#define PASSWD_SQLITE_ERROR_STRING "Unable to change password. SQLITE-ERROR: %s"
+#define LOG_SQLITE_ERROR_STRING "Unable to log user. SQLITE-ERROR: %s"
+
 
 bool auth_user(const char *,const char *, const char *);
 void change_pass(const char *,const char *, const char *);
@@ -37,14 +40,14 @@ bool auth_user(const char *dbfile, const char *username, const char *password)
   int rc = sqlite3_open(dbfile,&db);
 
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to open password database %s", dbfile);
+    syslog(LOG_CRIT, AUTH_SQLITE_ERROR_STRING, sqlite3_errmsg(db));
     return false;
   }
 
   rc = sqlite3_prepare_v2(db, QUERY_STRING, -1, &res, 0);
 
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to query database %s", dbfile);
+    syslog(LOG_CRIT, AUTH_SQLITE_ERROR_STRING, sqlite3_errmsg(db));
     sqlite3_close(db);
     return false;
   }
@@ -52,7 +55,7 @@ bool auth_user(const char *dbfile, const char *username, const char *password)
   rc = sqlite3_bind_text(res,1, username, strlen(username), SQLITE_STATIC);
   
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to query database %s", dbfile);
+    syslog(LOG_CRIT, AUTH_SQLITE_ERROR_STRING, sqlite3_errmsg(db));
     sqlite3_close(db);
     return false;
   }
@@ -87,14 +90,14 @@ void change_pass(const char *dbfile, const char *username, const char *password)
   int rc = sqlite3_open(dbfile,&db);
 
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to open password database %s", dbfile);
+    syslog(LOG_CRIT, PASSWD_SQLITE_ERROR_STRING, sqlite3_errmsg(db));
     return;
   }
 
   rc = sqlite3_prepare_v2(db, UPDATE_STRING, -1, &res, NULL);
 
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to query database %s", dbfile);
+    syslog(LOG_CRIT, PASSWD_SQLITE_ERROR_STRING,sqlite3_errmsg(db));
     sqlite3_close(db);
     return;
   }
@@ -104,7 +107,7 @@ void change_pass(const char *dbfile, const char *username, const char *password)
   
   
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to query database %s", dbfile);
+    syslog(LOG_CRIT, PASSWD_SQLITE_ERROR_STRING,sqlite3_errmsg(db));
     sqlite3_close(db);
     return;
   }
@@ -113,7 +116,7 @@ void change_pass(const char *dbfile, const char *username, const char *password)
 
   if(rc != SQLITE_OK)
     {
-      syslog(LOG_CRIT, "Unable to update password for user %s", username);
+      syslog(LOG_CRIT, PASSWD_SQLITE_ERROR_STRING,sqlite3_errmsg(db));
       sqlite3_close(db);
       return;
     }
@@ -139,7 +142,7 @@ void logSqlite(const char *dbfile, const char *username, const char* message){
   rc = sqlite3_prepare_v2(db, LOG_STRING, -1, &res, NULL);
 
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to open database %s", dbfile);
+    syslog(LOG_CRIT, LOG_SQLITE_ERROR_STRING, sqlite3_errmsg(db));
     sqlite3_close(db);
     return;
   }
@@ -148,7 +151,7 @@ void logSqlite(const char *dbfile, const char *username, const char* message){
   
   
   if(rc != SQLITE_OK){
-    syslog(LOG_CRIT, "Unable to query database %s", dbfile);
+    syslog(LOG_CRIT, LOG_SQLITE_ERROR_STRING, sqlite3_errmsg(db));
     sqlite3_close(db);
     return;
   }
@@ -157,7 +160,7 @@ void logSqlite(const char *dbfile, const char *username, const char* message){
 
   if(rc != SQLITE_OK)
     {
-      syslog(LOG_CRIT, "Unable to update log for user %s", username);
+      syslog(LOG_CRIT, LOG_SQLITE_ERROR_STRING, sqlite3_errmsg(db));
       sqlite3_close(db);
       return;
     }
@@ -171,112 +174,67 @@ void logSqlite(const char *dbfile, const char *username, const char* message){
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *handle, int flags, int argc,
 				   const char **argv)
 {
-	int pam_code;
+  int pam_code;
 
-	const char *username = NULL;
-	const char *password = NULL;
+  const char *username = NULL;
+  const char *password = NULL;
 
-	if(argc < 1){
-	  syslog(LOG_CRIT, "Unable to get db file name");
-	  return PAM_PERM_DENIED;
-	}
+  if(argc < 1){
+    syslog(LOG_CRIT, "Unable to get db file name");
+    return PAM_PERM_DENIED;
+  }
 
 
-	//pam_get_item(handle, PAM_USER, (const void**)&username);
+  //pam_get_item(handle, PAM_USER, (const void**)&username);
 	
-	/* Asking the application for an  username */
-	pam_code = pam_get_user(handle, &username, "USERNAME: ");
-	if (pam_code != PAM_SUCCESS) {
-	  syslog(LOG_ERR, "Can't get username");
-	  return PAM_PERM_DENIED;
-	}
+  /* Asking the application for an  username */
+  pam_code = pam_get_user(handle, &username, "USERNAME: ");
+  if (pam_code != PAM_SUCCESS) {
+    syslog(LOG_ERR, "Can't get username");
+    return PAM_PERM_DENIED;
+  }
 
-	/* Asking the application for a password */
-	pam_code =
-		pam_get_authtok(handle, PAM_AUTHTOK, &password, "PASSWORD: ");
-	if (pam_code != PAM_SUCCESS) {
-		syslog(LOG_ERR, "Can't get password");
-		return PAM_PERM_DENIED;
-	}
+  /* Asking the application for a password */
+  pam_code =
+    pam_get_authtok(handle, PAM_AUTHTOK, &password, "PASSWORD: ");
+  if (pam_code != PAM_SUCCESS) {
+    syslog(LOG_ERR, "Can't get password");
+    return PAM_PERM_DENIED;
+  }
 
-	syslog(LOG_INFO, "Attempting to authenticate %s in %s", username, argv[0]);
+  syslog(LOG_INFO, "Attempting to authenticate %s in %s", username, argv[0]);
 
 	
-	/* Checking the PAM_DISALLOW_NULL_AUTHTOK flag: if on, we can't accept empty passwords */
-	if (flags & PAM_DISALLOW_NULL_AUTHTOK) {
-		if (password == NULL || strcmp(password, "") == 0) {
-			syslog(LOG_ERR,
-				"Null authentication token is not allowed!.");
-			return PAM_PERM_DENIED;
-		}
-	}
+  /* Checking the PAM_DISALLOW_NULL_AUTHTOK flag: if on, we can't accept empty passwords */
+  if (flags & PAM_DISALLOW_NULL_AUTHTOK) {
+    if (password == NULL || strcmp(password, "") == 0) {
+      syslog(LOG_ERR,
+	     "Null authentication token is not allowed!.");
+      return PAM_PERM_DENIED;
+    }
+  }
 
-	/*Auth user reads a file with usernames and passwords and returns true if username
-	 * and password are correct. Obviously, you must not save clear text passwords */
-	if (auth_user(argv[0], username, password)) {
-		printf("Welcome, user");
-		return PAM_SUCCESS;
-	} else {
-		syslog(LOG_ERR, "Wrong username or password");
-		return PAM_PERM_DENIED;
-	}
+  /*Auth user reads a file with usernames and passwords and returns true if username
+   * and password are correct. Obviously, you must not save clear text passwords */
+  if (auth_user(argv[0], username, password)) {
+    printf("Welcome, %s\n", username);
+    return PAM_SUCCESS;
+  } else {
+    syslog(LOG_ERR, "Wrong username or password");
+    return PAM_PERM_DENIED;
+  }
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc,
 				const char **argv)
 {
-	/* /\* This struct contains the expiry date of the account *\/ */
-	/* struct tm expiry_date; */
-	/* expiry_date.tm_mday = 31; */
-	/* expiry_date.tm_mon = 12; */
-	/* expiry_date.tm_year = 2020; */
-	/* expiry_date.tm_sec = 0; */
-	/* expiry_date.tm_min = 0; */
-	/* expiry_date.tm_hour = 0; */
-
-	/* time_t expiry_time; */
-	/* time_t current_time; */
-
-	/* /\* Getting time_t value for expiry_date and current date *\/ */
-	/* expiry_time = mktime(&expiry_date); */
-	/* current_time = time(NULL); */
-
-	/* /\* Checking the account is not expired *\/ */
-	/* if (current_time > expiry_time) { */
-	/* 	return PAM_PERM_DENIED; */
-	/* } else { */
-	/* 	return PAM_SUCCESS; */
-	/* } */
   return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc,
 			      const char **argv)
 {
-	/* /\* Environment variable name *\/ */
-	/* const char *env_var_name = "USER_FULL_NAME"; */
-
-	/* /\* User full name *\/ */
-	/* const char *name = "John Smith"; */
-
-	/* /\* String in which we write the assignment expression *\/ */
-	/* char env_assignment[100]; */
-
-	/* /\* If application asks for establishing credentials *\/ */
-	/* if (flags & PAM_ESTABLISH_CRED) */
-	/* 	/\* We create the assignment USER_FULL_NAME=John Smith *\/ */
-	/* 	sprintf(env_assignment, "%s=%s", env_var_name, name); */
-	/* /\* If application asks to delete credentials *\/ */
-	/* else if (flags & PAM_DELETE_CRED) */
-	/* 	/\* We create the assignment USER_FULL_NAME, withouth equal, */
-	/* 	 * which deletes the environment variable *\/ */
-	/* 	sprintf(env_assignment, "%s", env_var_name); */
-
-	/* /\* In this case credentials do not have an expiry date, */
-	/*  * so we won't handle PAM_REINITIALIZE_CRED *\/ */
-
-	/* pam_putenv(pamh, env_assignment); */
-	return PAM_SUCCESS;
+  return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc,
@@ -294,87 +252,67 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc,
 
   logSqlite(argv[0], username, "LOGIN");
 
-  /* char dir_path[512]; */
-
-  /* /\* Get the username from PAM *\/ */
-  /* pam_get_item(pamh, PAM_USER, (const void **)&username); */
-
-  /* /\* Creating directory path string *\/ */
-  /* sprintf(dir_path, "/home/%s", username); */
-
-  /* mkdir(dir_path, 0770); */
-
   return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc,
 				    const char **argv)
 {
-	/* const char *username; */
-	/* char dir_path[512]; */
 
-	/* /\* Get the username from PAM *\/ */
-	/* pam_get_item(pamh, PAM_USER, (const void **)&username); */
+  const char *username;
 
-	/* /\* Creating directory path string *\/ */
-	/* sprintf(dir_path, "/home/%s", username); */
-
-	/* rmdir(dir_path); */
-
-	const char *username;
-
-	if(argc < 1){
-	  syslog(LOG_CRIT, "Unable to get db file name");
-	  return PAM_PERM_DENIED;
-	}
+  if(argc < 1){
+    syslog(LOG_CRIT, "Unable to get db file name");
+    return PAM_PERM_DENIED;
+  }
 
 
-	pam_get_item(pamh, PAM_USER, (const void**)&username);
+  pam_get_item(pamh, PAM_USER, (const void**)&username);
 
-	logSqlite(argv[0], username, "LOGOUT");
+  logSqlite(argv[0], username, "LOGOUT");
 
   
-	return PAM_SUCCESS;
+  return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc,
 				const char **argv)
 {
-	const char *username;
-	const char *cur_password;
-	const char *new_password;
-	/* We always return PAM_SUCCESS for the preliminary check */
-	if (flags & PAM_PRELIM_CHECK) {
-		return PAM_SUCCESS;
-	}
+  const char *username;
+  const char *cur_password;
+  const char *new_password;
+  /* We always return PAM_SUCCESS for the preliminary check */
+  if (flags & PAM_PRELIM_CHECK) {
+    return PAM_SUCCESS;
+  }
 
-	if(argc < 1){
-	  syslog(LOG_CRIT, "Unable to get db file name");
-	  return PAM_PERM_DENIED;
-	}
+  if(argc < 1){
+    syslog(LOG_CRIT, "Unable to get db file name");
+    return PAM_PERM_DENIED;
+  }
 
 
-	/* Get the username */
-	pam_get_item(pamh, PAM_USER, (const void **)&username);
+  /* Get the username */
+  pam_get_item(pamh, PAM_USER, (const void **)&username);
 
-	/* We're not handling the PAM_CHANGE_EXPIRED_AUTHTOK specifically
-	 * since we do not have expiry dates for our passwords. */
-	if ((flags & PAM_UPDATE_AUTHTOK) ||
-	    (flags & PAM_CHANGE_EXPIRED_AUTHTOK)) {
-		/* Ask the application for the password. From this module function, pam_get_authtok()
-		 * with item type PAM_AUTHTOK asks for the new password with the retype. Therefore,
-		 * to ask for the current password we must use PAM_OLDAUTHTOK. */
-		pam_get_authtok(pamh, PAM_OLDAUTHTOK, &cur_password,
-				"Insert current password: ");
+  /* We're not handling the PAM_CHANGE_EXPIRED_AUTHTOK specifically
+   * since we do not have expiry dates for our passwords. */
+  if ((flags & PAM_UPDATE_AUTHTOK) ||
+      (flags & PAM_CHANGE_EXPIRED_AUTHTOK)) {
+    /* Ask the application for the password. From this module function, pam_get_authtok()
+     * with item type PAM_AUTHTOK asks for the new password with the retype. Therefore,
+     * to ask for the current password we must use PAM_OLDAUTHTOK. */
+    pam_get_authtok(pamh, PAM_OLDAUTHTOK, &cur_password,
+		    "Insert current password: ");
 
-		if (auth_user(argv[0], username, cur_password)) {
-			pam_get_authtok(pamh, PAM_AUTHTOK, &new_password,
-					"New password: ");
-			change_pass(argv[0], username, new_password);
-		} else {
-			return PAM_PERM_DENIED;
-		}
-	}
-	return PAM_SUCCESS;
+    if (auth_user(argv[0], username, cur_password)) {
+      pam_get_authtok(pamh, PAM_AUTHTOK, &new_password,
+		      "New password: ");
+      change_pass(argv[0], username, new_password);
+    } else {
+      return PAM_PERM_DENIED;
+    }
+  }
+  return PAM_SUCCESS;
 }
 
